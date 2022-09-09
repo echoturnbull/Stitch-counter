c0cadc3bf72c4ac8466f9dc57916222ddbdd46ca
echoturnbull <a.m.turnbul@gmail.com>
2022-09-09 14:44:15 +1000

    Change2

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-let count = 5
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
 
-console.log(count)
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
